c51bdfc5098e7fd582e7ba694776c9c1b1007670
BachENSIM <ducbachng97@gmail.com>
2021-11-16 18:02:26 +0100

    modifier

--- TP1_API/src/test/java/SecteurTest.java
+++ TP1_API/src/test/java/SecteurTest.java
@@ -25,7 +25,7 @@ public class SecteurTest extends TestCase {
         System.out.println(sec1.getNombreAnimaux() + " -- " + sec2.getNombreAnimaux());
 
 
-        //assertEquals(sec2.compareTo(sec1),-1);
-        assertEquals(Secteur.secteurComparator().compare(sec1,sec2),-1);
+        assertEquals(sec2.compareTo(sec1),-1);//Comparable
+        assertEquals(Secteur.secteurComparator().compare(sec1,sec2),-1);//comparator
     }
 }
