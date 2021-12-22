83421e7d9b91c7e691067a2569c9a2926b836617
BachENSIM <94304662+BachENSIM@users.noreply.github.com>
2021-12-22 17:26:46 +0100

    Delete tp5 directory

--- tp5/src/main/java/mytp/com/example/tp5/model/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package mytp.com.example.tp5.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends CrudRepository<Address, Long> {
-
-}
